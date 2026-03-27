293f33f8be26f1e514031a6257e20b07ffa43e3f
Medhat-Alathmna <medhatalathmna@gmail.com>
2026-03-27 13:47:38 +0200

    fix robots

--- app/robots.ts
+++ app/robots.ts
@@ -8,7 +8,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: "*",
         allow: "/",
-        disallow: ["/api/", "/auth/", "/settings", "/map-settings"],
+        disallow: ["/api/", "/auth/"],
       },
     ],
     sitemap: `${SITE_URL}/sitemap.xml`,
